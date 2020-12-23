e8a90d4ca19712263b4f49cc1c5699459ff8d1f1
JW9506 <cf.dota@gmail.com>
2020-12-23 18:28:44 -0500

    support union type checking

--- src/typeLib.ts
+++ src/typeLib.ts
@@ -8,46 +8,60 @@ export type Primitives =
 
 export type AnyFunction<T extends any[] = any[], R = void> = (...args: T) => R;
 
-export type MapPrimitive<P extends Primitives> = P extends 'bigint'
+type ShiftArr<T extends string[] | string> = T extends [
+    _: any,
+    ...rest: infer R
+]
+    ? R
+    : T;
+type Inner<P> = P extends 'bigint'
     ? bigint
     : P extends 'boolean'
     ? boolean
+    : P extends 'undefined'
+    ? undefined
     : P extends 'number'
     ? number
     : P extends 'object'
     ? Record<string, unknown>
     : P extends 'string'
     ? string
-    : P extends 'function'
-    ? AnyFunction
-    : undefined;
+    : never;
+
+type Unwrap<T> = T extends any[] ? T[0] : T;
+
+export type MapPrimitive<P extends any[] | string, O = never> = {
+    0: MapPrimitive<ShiftArr<P>, O | Inner<Unwrap<P>>>;
+    1: O;
+    2: Inner<P>;
+}[P extends [] ? 1 : P extends string ? 2 : 0];
 
 export type Assert<S> = S extends Array<infer R>
-    ? R extends Record<string, Primitives>
+    ? R extends Record<string, Primitives | [Primitives, Primitives]>
         ? Array<
               {
                   [W in keyof R]: MapPrimitive<R[W]>;
               }
           >
         : never
-    : S extends Record<string, Primitives>
+    : S extends Record<string, Primitives | [Primitives, Primitives]>
     ? {
           [W in keyof S]: MapPrimitive<S[W]>;
       }
     : never;
 
 export type Keys<T> = T extends undefined
-    ? Record<string, Primitives>
+    ? Record<string, Primitives | [Primitives, Primitives]>
     : T extends Array<unknown>
-    ? Array<Record<string, Primitives>>
+    ? Array<Record<string, Primitives | [Primitives, Primitives]>>
     : T extends Array<infer R>
     ? Array<
           {
-              [K in keyof R]: Primitives;
+              [K in keyof R]: Primitives | [Primitives, Primitives];
           }
       >
     : {
-          [K in keyof T]: Primitives;
+          [K in keyof T]: Primitives | [Primitives, Primitives];
       };
 
 export type FilterObject<T> = T extends Array<infer R>
@@ -115,29 +129,86 @@ export function AssertType<U = undefined, T extends Keys<U> = Keys<U>>(
     : U extends Array<infer R>
     ? Array<FilterObject<R>>
     : FilterObject<U> {
-    if (Array.isArray(obj) !== Array.isArray(shape)) throw new TypeError(msg);
+    if (Array.isArray(obj) !== Array.isArray(shape))
+        throw new TypeError(
+            'Passed in value is expected to be of type "Array"'
+        );
     shape = Array.isArray(shape) ? shape[0] : shape;
     if (Array.isArray(obj)) {
         const _obj = obj;
         for (obj of _obj) {
             for (const key in shape) {
-                if (
-                    obj == null ||
-                    (obj as any)[key] == null ||
-                    typeof (obj as any)[key] !== shape[key]
-                ) {
-                    throw new TypeError(msg);
+                if (!Array.isArray(shape[key])) {
+                    if (
+                        obj == null ||
+                        (obj as any)[key] == null ||
+                        typeof (obj as any)[key] !== shape[key]
+                    ) {
+                        throw new TypeError(
+                            `${
+                                msg ? msg + ', ' : ''
+                            }${key} is expected to be of type "${
+                                shape[key]
+                            }", got ${typeof (obj as any)[key]}`
+                        );
+                    }
+                } else {
+                    const flag = (shape[key] as any[]).some((_key) => {
+                        return (
+                            obj != null &&
+                            (obj as any)[key] != null &&
+                            typeof (obj as any)[key] === _key
+                        );
+                    });
+                    if (!flag) {
+                        throw new TypeError(
+                            `${
+                                msg ? msg + ', ' : ''
+                            }${key} is expected to be of type "${(shape[
+                                key
+                            ] as any[]).join(
+                                ' | '
+                            )}", got ${typeof (obj as any)[key]}`
+                        );
+                    }
                 }
             }
         }
     } else {
         for (const key in shape) {
-            if (
-                obj == null ||
-                (obj as any)[key] == null ||
-                typeof (obj as any)[key] !== shape[key]
-            ) {
-                throw new TypeError(msg);
+            if (!Array.isArray(shape[key])) {
+                if (
+                    obj == null ||
+                    (obj as any)[key] == null ||
+                    typeof (obj as any)[key] !== shape[key]
+                ) {
+                    throw new TypeError(
+                        `${
+                            msg ? msg + ', ' : ''
+                        }${key} is expected to be of type "${
+                            shape[key]
+                        }", got ${typeof (obj as any)[key]}`
+                    );
+                }
+            } else {
+                const flag = (shape[key] as any[]).some((_key) => {
+                    return (
+                        obj != null &&
+                        (obj as any)[key] != null &&
+                        typeof (obj as any)[key] === _key
+                    );
+                });
+                if (!flag) {
+                    throw new TypeError(
+                        `${
+                            msg ? msg + ', ' : ''
+                        }${key} is expected to be of type "${(shape[
+                            key
+                        ] as any[]).join(' | ')}", got ${typeof (obj as any)[
+                            key
+                        ]}`
+                    );
+                }
             }
         }
     }
